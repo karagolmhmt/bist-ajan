28e82a31ad5a6595914c64f9d6239127a41981c5
Mehmet Karagöl <105505036+karagolmhmt@users.noreply.github.com>
2026-05-07 11:37:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ def gemini_istek(prompt, max_tokens=1000):
     if not api_key: return "HATA: API Anahtarı eksik."
 
     # Model ismi 1.5 Flash olarak güncellendi (Kota dostu)
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={api_key}"
+  url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={api_key}"
     
     payload = {
         "contents": [{"parts": [{"text": prompt}]}],
